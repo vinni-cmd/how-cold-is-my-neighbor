343bb9226987ddc63352448e467d973e9008db5b
vincentYoung <vinni.xander@gmail.com>
2023-02-01 07:52:34 -0700

    create footer and header components and stylesheet

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Created by <a href="https://vincentyoung.dev/" target="_blank">Vincent</a> at <a href="https://junocollege.com/" target="_blank">Juno College</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
