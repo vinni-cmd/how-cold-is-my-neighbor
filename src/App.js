eaacf5005917ec4d69d02ba64ac7f43e9d24be86
vincentYoung <vinni.xander@gmail.com>
2023-01-31 19:27:56 -0700

    build fetch structure with helper functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [userWeather, setUserWeather] = useState({});
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+      url.search = new URLSearchParams({
+        appid: '2f2490bb3753f1067ab2e758ffc26e39',
+        units: 'metric',
+        q: 'leduc',
+      });
+      try {
+        const data = await fetch(url);
+        const response = await data.json();
+        console.log(response);
+
+        const {
+          name: city,
+          sys: { country },
+          clouds: { all: cloudCover },
+          main: { temp },
+          main: { feels_like: feelTemp },
+          wind: { speed: windSpeed },
+        } = response;
+
+        function convertWindUnits(deg) {
+          const val = Math.floor((deg / 22.5) + 0.5);
+          const cardinals = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+          return cardinals[(val % 16)];
+        }
+
+        const windDirection = convertWindUnits(response.wind.deg);
+
+        const conditions = response.weather.map(condition => condition.description);
+
+        setUserWeather({
+          city,
+          country,
+          conditions,
+          temp,
+          feelTemp,
+          windSpeed,
+          windDirection,
+          cloudCover
+        });
+
+      } catch (error) {
+
+      }
+    }
+    fetchUserData();
+    // console.log(userWeather)
+  }, []);
+
+  console.log('userweather', userWeather);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How Cold Is My Neighbor</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// *** App Component ***
+// Create state items to hold data coming from the third-party API and the user input
+// - apiData: userCityData
+// - apiData: neighborCityData
+// - userQuery: userCity
+// - userQuery: neighborCity
+// - userQuery: weatherDetails {object consisting of user weather detail preferences}
+// is it better to split out the above into separate states or is a single state sufficient? Think I can just access the properties inside of the object when I need them.    
+
+// A local method (handleUserCityChange) to handle the onChange event to update state (userCity) with user input
+
+// Once the form is submitted, with valid entries (more error handling required), call the local method (getWeather) to submit an i) API call for the user city weather data and a ii) API call for the neighbor city weather data.
+
+// A local method (getWeather) to make the third-party API calls with user input
+// - when both calls are successful (handle with promise.all()), update the userCityData and neighborCityData (use below filter method here or after|just before passing details to result component???)
+// - if unsuccessful, display the error message
+
+// Filter method to extract weather details from returned data to match user's weather details selection
+
+// Render the application
+// - header
+// - form with user input
+// - use the imported Result component
+// - footer
+
+// *** Result Component ***
+// Create a component to display data from the third-party API
+// This component will get filtered weather data  and user and neighbor city name and passed in as props
