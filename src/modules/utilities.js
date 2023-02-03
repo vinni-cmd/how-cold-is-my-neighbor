855066eee986cd5a03f1d242439e6eee292de0d7
vincentYoung <vinni.xander@gmail.com>
2023-02-03 12:29:41 -0700

    add daylight property to city weather objects

--- src/modules/utilities.js
+++ src/modules/utilities.js
@@ -6,13 +6,30 @@ function calcWindDirection(degree) {
 }
 
 // https://www.geeksforgeeks.org/how-to-convert-unix-timestamp-to-time-in-javascript/
-function convertUnixToLocal(unix, timeZoneShift) {
-  const localTime = unix + timeZoneShift;
-  // convert to milliseconds and then create a new Date object
-  const dateObj = new Date(localTime * 1000);
-  const utcString = dateObj.toUTCString();
-  const time = utcString.slice(-12, -6);
+function getLocalTime(unix, timeZoneShift) {
+  const localDate = convertUnixToLocalDate(unix, timeZoneShift);
+  const utcString = localDate.toUTCString();
+  console.log('utcString', utcString)
+  const time = utcString.slice(-12, -7);
   return time;
 }
 
-export { calcWindDirection, convertUnixToLocal }
+function calcTimeDiffInMin(unix1, unix2, timeZoneShift) {
+  const localDate1 = convertUnixToLocalDate(unix1, timeZoneShift);
+  const localDate2 = convertUnixToLocalDate(unix2, timeZoneShift);
+  const localMinTime1 = localDate1.getHours() * 60 + localDate1.getMinutes();
+  const localMinTime2 = localDate2.getHours() * 60 + localDate2.getMinutes();
+  return (localMinTime2 - localMinTime1)
+}
+
+function convertUnixToLocalDate(unix, timeZoneShift) {
+  const localTime = unix + timeZoneShift;
+  // convert to milliseconds and then create a new Date object
+  const dateObj = new Date(localTime * 1000);
+  console.log('dateObj', dateObj);
+  return dateObj
+}
+
+
+
+export { calcWindDirection, getLocalTime, calcTimeDiffInMin }
