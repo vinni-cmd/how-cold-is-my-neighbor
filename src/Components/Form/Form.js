125996c2c98eee6603841808a54cf1a0afbac9aa
vincentYoung <vinni.xander@gmail.com>
2023-02-03 10:41:24 -0700

    add datalist to city inputs

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -37,9 +37,30 @@ const Form = ({ handleFormSubmission }) => {
       <fieldset>
         <legend>Select your locations</legend>
         <label htmlFor="userCity">Your location: </label>
-        <input type="text" name="userCity" id="userCity" onChange={(e) => { setUserCity(e.target.value) }} value={userCity} required />
+        <input type="text" name="userCity" id="userCity" onChange={(e) => { setUserCity(e.target.value) }} value={userCity} required list='commonLocations' />
         <label htmlFor="neighborCity">Your neighbor's location: </label>
-        <input type="text" name="neighborCity" id="neighborCity" value={neighborCity} onChange={(e) => { setNeighborCity(e.target.value) }} required />
+        <input type="text" name="neighborCity" id="neighborCity" value={neighborCity} onChange={(e) => { setNeighborCity(e.target.value) }} required list='commonLocations' />
+        <datalist id='commonLocations'>
+          <option value="Toronto"></option>
+          <option value="Montreal"></option>
+          <option value="Vancouver"></option>
+          <option value="Quebec City"></option>
+          <option value="Calgary"></option>
+          <option value="Ottawa"></option>
+          <option value="Winnipeg"></option>
+          <option value="Halifax"></option>
+          <option value="Victoria"></option>
+          <option value="Edmonton"></option>
+          <option value="St. John's"></option>
+          <option value="Saskatoon"></option>
+          <option value="Regina"></option>
+          <option value="Saint John"></option>
+          <option value="Charlottetown"></option>
+          <option value="Whitehorse"></option>
+          <option value="Yellowknife"></option>
+          <option value="Iqaluit"></option>
+          <option value="Fredericton"></option>
+        </datalist>
       </fieldset>
       <fieldset>
         <legend>Weather details</legend>
