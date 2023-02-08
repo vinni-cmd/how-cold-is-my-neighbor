3f2006378376aee63a4c02386113c1bea824487e
vincentYoung <vinni.xander@gmail.com>
2023-02-08 07:02:31 -0700

    add scroll-to effect for error and results

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,13 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { RiThunderstormsFill } from 'react-icons/ri';
 
 const Error = ({ errorMessage, setApiCallInProgress }) => {
+  const scollToRef = useRef(null);
+
   useEffect(() => {
     setApiCallInProgress(false);
   }, [setApiCallInProgress]);
 
+  useEffect(() => {
+    scollToRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [scollToRef]);
+
   return (
-    <div className="error">
+    <div className="error" ref={scollToRef}>
       <RiThunderstormsFill className='error-icon' />
       <p>{errorMessage}</p>
     </div>
--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import CityResult from './CityResult';
 import Comparison from './Comparison';
 
 const Results = ({ userWeather, uncleWeather, weatherSelection, setApiCallInProgress }) => {
+  const scollToRef = useRef(null);
+
   useEffect(() => {
     setApiCallInProgress(false);
   }, [setApiCallInProgress]);
+
+  useEffect(() => {
+    scollToRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [scollToRef]);
+
   return (
-    <section className="results">
+    <section className="results" ref={scollToRef}>
       <h2>Results</h2>
       <div className="results-breakdown">
         <CityResult cityWeather={userWeather} weatherSelection={weatherSelection} />
